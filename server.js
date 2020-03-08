5a2d6246777106d9b4fc0a22d77227232a644590
DAVICORREIA21 <davicorreia21@gmail.com>
2020-03-07 22:44:07 -0300

    Criação da rota

--- server.js
+++ server.js
@@ -2,5 +2,9 @@
 const express = require('express');
 //Executando a função
 const app = express();
+//Acessar a rota , rota raiz "/",funções de requisição ao servidor , a resposta que vai se dar a requisição
+app.get('/',(req,rest)=>{
+  rest.send("Hello Davi");
+})
 //Aplicação usar porta 3001
 app.listen(3001);
